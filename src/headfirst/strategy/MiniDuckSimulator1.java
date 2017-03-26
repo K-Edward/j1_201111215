0c69b4aabb780700ccf35158f0c2207349d10a8e
K-Edward <hwan0092@naver.com>
2017-03-26 19:35:08 +0900

    Add files via upload

--- src/headfirst/strategy/MiniDuckSimulator1.java
+++ src/headfirst/strategy/MiniDuckSimulator1.java
@@ -5,9 +5,9 @@ public class MiniDuckSimulator1 {
   public static void main(String[] args) {
     
     Duck mallard = new MallardDuck();
-    mallard.display();
-    mallard.performQuack();
-    mallard.performFly();
+    mallard.display(); //오리 정체 밝힘
+    mallard.performQuack(); //오리가 우는 방식
+    mallard.performFly(); //오리가 날아가는 방식
     System.out.println("");
     
     
@@ -16,7 +16,7 @@ public class MiniDuckSimulator1 {
     model.display();
     model.performQuack();
     //model.performFly();
-    //model.setFlyBehavior(new FlyRocketPowered());
+    //model.setFlyBehavior(new FlyRocketPowered()); //원래 FlyNoWays 였던 날아가는 방식을 FlyRocketPowered로 바꿔주는 코드
     model.performFly();
     System.out.println("");
 
@@ -31,6 +31,15 @@ public class MiniDuckSimulator1 {
     rubber.performQuack();
     rubber.performFly();
     System.out.println("");
+    
+    Duck decoy = new DecoyDuck();
+    decoy.display();
+    decoy.performQuack();
+    decoy.performFly();
+    System.out.println("");
+    
+    
+    decoy.swim();
     
   }
 }
