e88d5a4ca9883bf833e5b580b9b66781a5396491
K-Edward <hwan0092@naver.com>
2017-03-26 19:17:26 +0900

    Delete FlyWithWings.java

--- src/src/headfirst/strategy/FlyWithWings.java
+++
@@ -1,7 +0,0 @@
-package headfirst.strategy;
-
-public class FlyWithWings implements FlyBehavior {
-  public void fly() {
-    System.out.println("I'm flying!!"); //실제로 날아가는걸 프로그래밍하지는 않고 print문 사용
-  }
-}
