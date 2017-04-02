44e372f2579d03cf9d098fa88717cb0fb6e52448
K-Edward <hwan0092@naver.com>
2017-04-02 21:56:58 +0900

    Add files via upload

--- src/com/sd/gui/CalcAreaListener.java
+++ src/com/sd/gui/CalcAreaListener.java
@@ -5,21 +5,18 @@ import javax.swing.*;
 
 public class CalcAreaListener implements ActionListener {
   public void actionPerformed(ActionEvent e) {
+    System.out.println("Hello!!!!"); //리스너 작동 확인용.
+    
     JButton b = (JButton)e.getSource();
-    JTextField r1 = (JTextField)e.getSource();
-    JTextField r2 = (JTextField)e.getSource();
+    
+    JTextField radius = (JTextField)e.getSource();
+    
+    System.out.println(radius);
     
     String cmd = e.getActionCommand();
-    int radius;
-    double area;
-    String result;
     
     if(cmd.equals("계산")) {
       System.out.println("계산 버튼");
-      radius = Integer.parseInt(r1.getText());
-      area = radius*radius*3.141592;
-      result = area+"";
-      r2.setText(result);
       b.setText("계산했습니다.");
     }
     else if(cmd.equals("취소")) {
