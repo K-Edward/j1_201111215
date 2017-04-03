34229a31f18a1c70331e1aabfa41f82a6da427a9
K-Edward <hwan0092@naver.com>
2017-04-04 02:24:43 +0900

    Add files via upload

--- src/headfirst/observer/weather/ForecastDisplay.java
+++ src/headfirst/observer/weather/ForecastDisplay.java
@@ -1,9 +1,10 @@
 package headfirst.observer.weather;
-
+//예보에 관한 Display
+//pressure의 변화를 통해서 날씨가 좋아질지, 변함 없을지, 비가 오게 될지 예보 알림을 줘야함.
 public class ForecastDisplay implements Observer, DisplayElement {
-  private float temperature;
-  private float humidity;
-  private float pressure;
+  private float current_Pressure;
+  private float last_Pressure;
+  private int count;
   private Subject weatherData;
   
   public ForecastDisplay(Subject weatherDate){
@@ -12,15 +13,29 @@ public class ForecastDisplay implements Observer, DisplayElement {
   }
   
   public void update(float temperature, float humidity, float pressure) {
-    this.temperature = temperature;
-    this.humidity = humidity;
-    this.pressure = pressure;
+    count++;
+    if(count == 1){
+      last_Pressure = pressure;
+      current_Pressure = pressure;
+    }
+    else{
+      last_Pressure = current_Pressure;
+      current_Pressure = pressure;
+    }
+    
     display();
   }
   
   public void display(){
+    System.out.println("Forecast :) ");
+    if(current_Pressure > last_Pressure)
+      System.out.println("고기압의 영향으로 날씨가 좋아질 예정입니다 ! ^_^");
+    else if(current_Pressure == last_Pressure)
+      System.out.println("현재 날씨가 유지될 전망입니다.");
+    else if(current_Pressure < last_Pressure)
+      System.out.println("저기압의 영향으로 비가 오겠습니다. 기온이 낮아질 수 있으니 유의하세요 !");
     
-    System.out.println("Forecast conditions: " + temperature + "F degrees and " + humidity + "% humidity and " + pressure + " pressure.");
+    System.out.println("");
   }
   
 }
--- src/headfirst/observer/weather/StatisticsDisplay.java
+++ src/headfirst/observer/weather/StatisticsDisplay.java
@@ -1,9 +1,10 @@
 package headfirst.observer.weather;
 
 public class StatisticsDisplay implements Observer, DisplayElement {
-  private float temperature;
-  private float humidity;
-  private float pressure;
+  private float max_Temperature;
+  private float min_Temperature;
+  private float sum_Temperature;
+  private int count; //데이터가 들어온 횟수를 카운트할 변수
   private Subject weatherData;
   
   public StatisticsDisplay(Subject weatherDate){
@@ -12,15 +13,30 @@ public class StatisticsDisplay implements Observer, DisplayElement {
   }
   
   public void update(float temperature, float humidity, float pressure) {
-    this.temperature = temperature;
-    this.humidity = humidity;
-    this.pressure = pressure;
+    // 여기서 통계를 내야함.
+    // 통계 내용은 평균 온도와 최고 온도, 최저 온도를 구해야 함
+    sum_Temperature = sum_Temperature + temperature;
+    count++;
+    
+    if(count == 1){
+      max_Temperature = temperature;
+      min_Temperature = temperature;
+    }
+    else{
+      if(temperature > max_Temperature){
+        max_Temperature = temperature;
+      }
+      
+      if(min_Temperature > temperature){
+        min_Temperature = temperature;
+      }
+    }
+    
     display();
   }
   
   public void display(){
-    
-    System.out.println("Statistics conditions: " + temperature + "F degrees and " + humidity + "% humidity and " + pressure + " pressure.");
+    System.out.println("Statistics - Average temperature : " + (sum_Temperature/count) + "F, Max temperature : " + (max_Temperature) + "F, Minimum temperature : " + (min_Temperature) + "F");
   }
   
 }
